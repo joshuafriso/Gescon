be9a2b912f0e09e1bf7e33f7baf8f82eb2686228
Joshua Friso <joshuafriso@hotmail.com>
2016-09-28 20:16:08 -0300

    Adicionado plist google e frameworks

---
+++ Carthage/Checkouts/Firebase-Carthage/Firebase/Firebase-umbrella.h
@@ -0,0 +1,25 @@
+//
+//  Firebase-umbrella.h
+//  Firebase
+//
+//  Created by jbrooks on 3/7/16.
+//  Copyright © 2016 jbrooks. All rights reserved.
+//
+
+
+#import "FAuthData.h"
+#import "FAuthType.h"
+#import "FConfig.h"
+#import "FDataSnapshot.h"
+#import "FEventType.h"
+#import "Firebase.h"
+#import "FirebaseApp.h"
+#import "FirebaseServerValue.h"
+#import "FMutableData.h"
+#import "FQuery.h"
+#import "FTransactionResult.h"
+
+
+FOUNDATION_EXPORT double FirebaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char FirebaseVersionString[];
+
---
+++ Carthage/Checkouts/Firebase-Carthage/README.md
@@ -0,0 +1,8 @@
+# Firebase-Carthage
+
+Unofficial distribution of [Firebase iOS SDK](https://www.firebase.com) as a Carthage framework.
+
+To include, add the following line to your `Cartfile`:
+   
+    github "jonbrooks/Firebase-Carthage.git" "master"
+ 
